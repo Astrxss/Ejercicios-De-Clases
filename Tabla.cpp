1a400df60d02f9e0d5d16724e4a1ff6aca52be6b
Astrxss <164410596+Astrxss@users.noreply.github.com>
2024-05-06 12:33:09 -0600

    Add files via upload

---
+++ Tabla.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(){
+    
+    int i=1; 
+    while (i<13)
+    {
+
+        cout << "12 * "<< i <<"  =  "<< i * 12 << endl;
+        i++;
+
+    }
+    return 0;
+}
